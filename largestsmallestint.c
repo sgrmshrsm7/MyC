858a4cc2dd044358c544ea9685bcc99cd3abcda0
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-09-15 14:33:39 +0530

    largest and smallest int

---
+++ largestsmallestint.c
@@ -0,0 +1,14 @@
+//Calculating largest and smallest int
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 1;
+    while (a > 0) {
+        a++;
+    }
+    printf("\nLargest int number is : %d\n" , a-1);
+    printf("Smallest int number is : %d\n", a);
+    return 0;
+}
