f6b0cd2de8606f6ccb95150bdc544baf7e2fe2bd
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-09-18 17:04:29 +0530

    gcd

---
+++ s3/gcd.c
@@ -0,0 +1,26 @@
+//Calculating GCD of two numbers
+
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+    int ans;
+    if (a % b == 0)
+    {
+        ans = b;
+    }
+    else
+    {
+        ans = gcd(b,a%b);
+    }
+    return ans;
+}
+
+int main()
+{
+    int a,b;
+    printf("Enter two numbers : ");
+    scanf("%d%d",&a,&b);
+    printf("GCD is : %d" , gcd(a,b));
+    return 0;
+}
