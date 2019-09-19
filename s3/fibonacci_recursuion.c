2d9a590cfce351534396ef8d059d032eb88ab4e6
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-09-19 22:03:52 +0530

    fibonacci_recursion.c

---
+++ s3/fibonacci_recursuion.c
@@ -0,0 +1,23 @@
+//Calculating nth term of fibonacci series using recursion
+#include <stdio.h>
+int fib(int n)
+{
+        int ans;
+        if (n==1||n==2)
+        {
+                ans=1;
+        }
+        else
+        {
+                ans=fib(n-1)+fib(n-2);
+        }
+        return ans;
+}
+int main()
+{
+        int n;
+        printf("Enter number(n) : ");
+        scanf("%d",&n);
+        printf("nth fibonacci term is : %d",fib(n));
+        return 0;
+}
