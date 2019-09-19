ff0c7636b7f8e68907f8352fa0f90a52e33b5a86
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-09-19 20:10:20 +0530

    optimization

--- s3/pow_recursion.c
+++ s3/pow_recursion.c
@@ -1,27 +1,30 @@
 //Calculating power recursion
+
 #include <stdio.h>
-long long int power(int x, int n)
+
+long long int power(int x , int n)
 {
         long long int ret_val, xpowbytwo;
-        if(n==0)
-        ret_val=1;
-        else if(n==1)
-        ret_val=x;
+        if(n == 0)
+        ret_val = 1;
+        else if(n == 1)
+        ret_val = x;
         else
         {
-                xpowbytwo=power(x,n/2);
-                ret_val=xpowbytwo*xpowbytwo;
-                if(n%2==1)
+                xpowbytwo=power(x , n / 2);
+                ret_val = xpowbytwo*xpowbytwo;
+                if(n % 2 == 1)
                 {
-                        ret_val=ret_val*x;
+                        ret_val = ret_val*x;
                 }
         }
         return ret_val;
 }
+
 int main()
 {
         int x,n;
         printf("Enter value of x and n : ");
         scanf("%d%d",&x,&n);
-        printf("pow(x,n) = %lld",power(x,n));
+        printf("pow(x,n) = %lld",power(x , n));
 }
