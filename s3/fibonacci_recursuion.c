6cf7b41f006d9007cde42bd20f26fb3c3e3d0813
Sagar Mishra <sgrmshrsm7@gmail.com>
2019-09-19 22:05:00 +0530

    fibonacci_recursion.c

--- s3/fibonacci_recursuion.c
+++ s3/fibonacci_recursuion.c
@@ -1,18 +1,21 @@
 //Calculating nth term of fibonacci series using recursion
+
 #include <stdio.h>
+
 int fib(int n)
 {
         int ans;
-        if (n==1||n==2)
+        if (n == 1 || n == 2)
         {
-                ans=1;
+                ans = 1;
         }
         else
         {
-                ans=fib(n-1)+fib(n-2);
+                ans = fib(n - 1) + fib(n - 2);
         }
         return ans;
 }
+
 int main()
 {
         int n;
